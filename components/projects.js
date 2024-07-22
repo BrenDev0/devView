db1b757e0ce30197f8235758983628736c31ee90
Brendan Lahey <lahey1991@gmail.com>
2024-07-21 19:47:57 -0500

    first commit

---
+++ components/projects.js
@@ -0,0 +1,9 @@
+export const projectList = () => {
+   
+
+   return "test"
+}
+
+
+
+
---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
+const db = require('./database/models.js')
+
+
+const CreateWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+
+
+app.whenReady().then(() => {
+
+    CreateWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            
+            CreateWindow()
+        }
+    })
+})
+
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin'){
+        app.quit()
+    }
+})
