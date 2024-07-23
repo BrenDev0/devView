c94fb7b733251388f8ed6e88ec9cf91596fda75f
Brendan Lahey <lahey1991@gmail.com>
2024-07-23 10:51:40 -0500

    changes: delete cascade to db models

--- database/models.js
+++ database/models.js
@@ -8,9 +8,9 @@ const subComponentsDb = new sqlite3.Database('./database/subComponents.db')
 db.serialize(() => {
     db.run("CREATE TABLE IF NOT EXISTS projects (projectid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, language TEXT, status TEXT, description TEXT)")
     compopnentDb.get("PRAGMA foreign_keys = ON")
-    compopnentDb.run("CREATE TABLE IF NOT EXISTS components (componentid INTEGER PRIMARY KEY AUTOINCREMENT, projectid INTEGER NOT NULL, name TEXT, status TEXT, description TEXT, branch TEXT, branchPaths TEXT, FOREIGN KEY (projectid) REFERENCES projects (projectid))")
+    compopnentDb.run("CREATE TABLE IF NOT EXISTS components (componentid INTEGER PRIMARY KEY AUTOINCREMENT, projectid INTEGER NOT NULL, name TEXT, status TEXT, description TEXT, branch TEXT, branchPaths TEXT, FOREIGN KEY (projectid) REFERENCES projects (projectid) ON DELETE CASCADE)")
     subComponentsDb.get("PRAGMA foreign_keys = ON")
-    subComponentsDb.run("CREATE TABLE IF NOT EXISTS subcomponents (subid INTEGER PRIMARY KEY AUTOINCREMENT, componentid INTEGER NOT NULL, name TEXT, status TEXT, decription TEXT, branch TEXT, branchPaths TEXT, FOREIGN KEY (componentid) REFERENCES components (componentid))")
+    subComponentsDb.run("CREATE TABLE IF NOT EXISTS subcomponents (subid INTEGER PRIMARY KEY AUTOINCREMENT, componentid INTEGER NOT NULL, name TEXT, status TEXT, decription TEXT, branch TEXT, branchPaths TEXT, FOREIGN KEY (componentid) REFERENCES components (componentid) ON DELETE CASCADE)")
 })
 
 module.exports = { db, compopnentDb, subComponentsDb}
