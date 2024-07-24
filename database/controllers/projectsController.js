82aff27937998ebdd07865e4c4f4b25c2402ab2c
Brendan Lahey <lahey1991@gmail.com>
2024-07-24 11:27:40 -0500

    bug: update project switch statement
    switch statment was incorrectly coded and is now resolved and functional

--- database/controllers/projectsController.js
+++ database/controllers/projectsController.js
@@ -49,40 +49,40 @@ const deleteProject = (event, id) => {
 const updateProjectData = (event, data) => {
     let stmt = ""
     switch (data.col) {
-        case data.col === "NAME":
+        case "NAME":
             stmt = ("UPDATE projects SET name = ? WHERE projectid = ?")
             db.run(stmt, [data.value, data.id], (err) => {
                 if (err){
                     return console.log(err.message)
                 }
-                return console.log(`Updated ${this.changes}`)
+                return console.log('Updated: Name')
             })
             break
-        case data.col === "LANGUAGE":
+        case "LANGUAGE":
             stmt = ("UPDATE projects SET language = ? WHERE projectid = ?")
             db.run(stmt, [data.value, data.id], (err) => {
                 if (err) {
                     return console.log(err.message)
                 }
-                return console.log(`Updatae: ${this.changes}`)
+                return console.log(`Updated: Language`)
             }) 
             break
-        case data.col === "STATUS":
+        case "STATUS":
             stmt = ("UPDATE projects SET status = ?  WHERE projectid = ?") 
             db.run(stmt, [data.value, data.id], (err) => {
                 if (err) {
                     return console.log(err.message)
                 }
-                return console.log(`Update: ${this.changes}`)
+                console.log('Updated: Status')
             }) 
             break
-        case data.col === "DESCRIPTION":
+        case "DESCRIPTION":
             stmt = ("UPDATE projects SET description = ? WHERE projectid = ?")
             db.run(stmt, [data.value, data.id], (err) => {
                 if (err){
                     return console.log(err.message)
                 }
-                return console.log(`Update: ${this.changes}`)
+                return console.log('Updated: Discription')
             }) 
             break     
         default:
