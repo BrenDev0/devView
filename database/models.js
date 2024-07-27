5c3e90ac74378894840b28f62b22322a9f02adb8
Brendan Lahey <lahey1991@gmail.com>
2024-07-27 15:35:16 -0500

    change: removed table columns from component table
    branch and branch path cols removed

--- database/models.js
+++ database/models.js
@@ -8,9 +8,9 @@ const subComponentsDb = new sqlite3.Database('./database/tables/subComponents.db
 db.serialize(() => {
     db.run("CREATE TABLE IF NOT EXISTS projects (projectid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, language TEXT, status TEXT, description TEXT)")
     compopnentsDb.get("PRAGMA foreign_keys = ON")
-    compopnentsDb.run("CREATE TABLE IF NOT EXISTS components (componentid INTEGER PRIMARY KEY AUTOINCREMENT, projectid INTEGER NOT NULL, name TEXT, status TEXT, description TEXT, branch TEXT, branchPaths TEXT, FOREIGN KEY (projectid) REFERENCES projects (projectid) ON DELETE CASCADE)")
+    compopnentsDb.run("CREATE TABLE IF NOT EXISTS components (componentid INTEGER PRIMARY KEY AUTOINCREMENT, projectid INTEGER NOT NULL, name TEXT, status TEXT, description TEXT, FOREIGN KEY (projectid) REFERENCES projects (projectid) ON DELETE CASCADE)")
     subComponentsDb.get("PRAGMA foreign_keys = ON")
-    subComponentsDb.run("CREATE TABLE IF NOT EXISTS subcomponents (subid INTEGER PRIMARY KEY AUTOINCREMENT, componentid INTEGER NOT NULL, name TEXT, status TEXT, decription TEXT, branch TEXT, branchPaths TEXT, FOREIGN KEY (componentid) REFERENCES components (componentid) ON DELETE CASCADE)")
+    subComponentsDb.run("CREATE TABLE IF NOT EXISTS subcomponents (subid INTEGER PRIMARY KEY AUTOINCREMENT, componentid INTEGER NOT NULL, name TEXT, status TEXT, decription TEXT, FOREIGN KEY (componentid) REFERENCES components (componentid) ON DELETE CASCADE)")
 })
 
 module.exports = { db, compopnentsDb, subComponentsDb}
