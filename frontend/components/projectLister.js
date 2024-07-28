bacc3a4ba46aed0168dfb18cdc75eb8c9583b7cd
Brendan Lahey <lahey1991@gmail.com>
2024-07-28 13:06:10 -0500

    WIP: li attributes
    added class and value attributes to li

--- frontend/components/projectLister.js
+++ frontend/components/projectLister.js
@@ -5,7 +5,10 @@ export const projectLister = (projects) => {
    
    projects.forEach((project) => {
       let li = document.createElement('li')
+      li.setAttribute('class', 'project')
+      li.setAttribute('value', project.projectid)
       li.appendChild(document.createTextNode(project.name))
+      
       ul.appendChild(li)
    })
 
