f9563896019f34ae9fbbf06b7468d42c6b6ab72c
Brendan Lahey <lahey1991@gmail.com>
2024-07-28 13:01:16 -0500

    context variables
    using context for project list

--- renderer.js
+++ renderer.js
@@ -1,6 +1,8 @@
 import { projectLister } from './frontend/components/projectLister.js'  
+import { projectContext, actions } from './frontend/context/projectsContext.js'
 
 const projects = await window.projectsAPI.getAllProjects()
+actions.setProjects(projects)
 
-projectLister(projects)
+projectLister(projectContext.projects)
 
