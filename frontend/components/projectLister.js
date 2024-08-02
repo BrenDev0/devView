e24c123c28306b243ffcf5493c51fb1445fa7d97
Brendan Lahey <lahey1991@gmail.com>
2024-08-01 19:58:46 -0500

    ignore

--- frontend/components/projectLister.js
+++ frontend/components/projectLister.js
@@ -1,15 +1,18 @@
 import { highlight } from "../utils/highlight.js"
+import { tabsContext, actions } from "../context/tabsContext.js"
 
 export const projectLister = (projects) => {
    const ul = document.getElementById('projects')
 
-   
    projects.forEach((project) => {
       let li = document.createElement('li')
       li.setAttribute('class', 'project')
       li.setAttribute('value', project.projectid)
       li.appendChild(document.createTextNode(project.name))
-      li.addEventListener('click', (e) => highlight(e, 'project'))
+      li.addEventListener('click', (e) => {
+         highlight(e, 'project');
+         actions.addTab(project)
+      })
       
       ul.appendChild(li)
    })
