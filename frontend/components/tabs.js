44d04ac677e8f5bd04923a28947f5eb8dfefaeed
Brendan Lahey <lahey1991@gmail.com>
2024-08-06 13:26:39 -0500

    WIP: toolbar

--- frontend/components/tabs.js
+++ frontend/components/tabs.js
@@ -2,19 +2,39 @@ import { tabsContext, actions } from "../context/tabsContext.js"
 
 export const tabs = () => {
     const con = document.getElementById('tabs')
-    const current = tabsContext.tabs
-    console.log(current)
-
+    
     const interval = () => {
         const check = setInterval(() => { 
-            // for(let i = 0; i < tabsContext.tabs.length; i++){
-            //     let button = document.createElement('button')
-            //     button.setAttribute('class', 'tab')
-            //     button.setAttribute('value', tabsContext.tabs[i].name)
-            //     button.appendChild(document.createTextNode(tabsContext.tabs[i].name))
+            let currentTabs =  document.getElementsByClassName('tab')
+
+            if(currentTabs.length !== tabsContext.tabs.length ){
+                for(let i = 0; i < tabsContext.tabs.length; i++){
+                    if(currentTabs.length === 0){
+                        const button = document.createElement('button')
+                        button.setAttribute('class', 'tab')
+                        button.setAttribute('value', tabsContext.tabs[i].name)
+                        button.appendChild(document.createTextNode(tabsContext.tabs[i].name))
+
+                        con.appendChild(button)
+                    }
+                    else {
+                        for (let j = 0; j < currentTabs.length; j++){
+                            if (tabsContext.tabs[i].name !== currentTabs[j].value){
+                                const button = document.createElement('button')
+                                button.setAttribute('class', 'tab')
+                                button.setAttribute('value', tabsContext.tabs[i].name)
+                                button.appendChild(document.createTextNode(tabsContext.tabs[i].name))
+    
+                                return con.appendChild(button)
+                            }
+                            else {
+                               null
+                            }
+                        }
+                    }
+                }
+            }
             
-            //     con.appendChild(button)
-            // } 
         }, 500)
     
         return () => clearInterval(check)
--- renderer.js
+++ renderer.js
@@ -1,10 +1,16 @@
 import { projectLister } from './frontend/components/projectLister.js'  
 import { tabs } from './frontend/components/tabs.js'
+import { toolbar } from './frontend/components/toolbar.js'
 import { projectContext, actions } from './frontend/context/projectsContext.js'
 
+//toolbar 
+toolbar()
+
+//get projects
 const projects = await window.projectsAPI.getAllProjects()
 actions.setProjects(projects)
 
+//list projects
 projectLister(projectContext.projects)
 tabs()
 
